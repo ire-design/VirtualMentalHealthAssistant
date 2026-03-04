c3e87fbf4dd46abd9f2f68b3ac11828a2c0ea94c
Irene Musau <irenemwikalii04@gmail.com>
2026-03-04 10:21:46 +0300

    Add history parameter to sendMessage function

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:5000';
 
-export const sendMessage = async (message) => {
+export const sendMessage = async (message, history) => {
     try{
         const response = await axios.post(`${API_BASE_URL}/chat`, {
-            message: message
+            message: message,
+            history: history
         });
         return response.data.reply;
     } catch (error){
